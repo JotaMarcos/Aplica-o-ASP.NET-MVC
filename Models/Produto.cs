3624259d89baba770583c547f497e134f4f22caf
JotaMarcos <jmarcos_duarte@hotmail.com>
2020-06-29 15:46:32 -0400

    Implementada primeira aplicação em ASP.NET com MVC.

---
+++ Models/Produto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CursoMV.Models
+{
+    public class Produto
+    {
+        
+        public int Id { get; set; }
+
+        [Display(Name = "Descrição")]
+        [Required(ErrorMessage = "O campo descrição é obrigatório!")]
+        public string Descricao { get; set; }
+
+        [Display(Name = "Quantidade")]
+        [Required(ErrorMessage = "O campo quantidade é obrigatório!")]
+        [Range(1, 100, ErrorMessage = "Valor fora da faixa permitida!")]
+        public int Quatidade { get; set; }
+        
+
+
+        public int CategoriaId { get; set; }
+        public Categoria Categoria { get; set; }
+
+    }
+}
